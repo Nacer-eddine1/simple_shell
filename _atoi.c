383cd31a1a61bde7e5ba0090c116cba3bcd54960
Nacer-eddine Kheireddine <109687458+Nacer-eddine1@users.noreply.github.com>
2023-05-20 19:21:31 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,74 +1,78 @@
 #include "shell.h"
+#include <unistd.h>
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * interactive - Checks if the shell is running in interactive mode.
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if in interactive mode, 0 otherwise.
  */
-int interactive(info_t *info)
+int interactive(void)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-
-/**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (isatty(STDIN_FILENO) && STDIN_FILENO <= 2)
 		return (1);
 	else
 		return (0);
 }
 
 /**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ * is_delim - Checks if a character is a delimiter.
+ * @c: The character to check.
+ * @delim: The delimiter string.
+ *
+ * Return: 1 if the character is a delimiter, 0 otherwise.
  */
+int is_delim(char c, char *delim)
+{
+	while (*delim != '\0')
+	{
+		if (*delim == c)
+			return (1);
+		delim++;
+	}
+	return (0);
+}
 
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is alphabetic, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer value.
+ */
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	if (s[i] == '-')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
+		sign = -1;
+		i++;
 	}
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
 
-	return (output);
+	return (result * sign);
 }
