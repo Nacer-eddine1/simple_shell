5a234ab13f7a1c7baf89eb303c24dc4e86e56bf2
Nacer-eddine Kheireddine <109687458+Nacer-eddine1@users.noreply.github.com>
2023-05-20 19:46:03 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -16,16 +16,16 @@ int interactive(void)
 
 /**
  * is_delim - Checks if a character is a delimiter.
- * @c: The character to check.
+ * @ch: The character to check.
  * @delim: The delimiter string.
  *
  * Return: 1 if the character is a delimiter, 0 otherwise.
  */
-int is_delim(char c, char *delim)
+int is_delim(char ch, char *delim)
 {
 	while (*delim != '\0')
 	{
-		if (*delim == c)
+		if (*delim == ch)
 			return (1);
 		delim++;
 	}
@@ -34,38 +34,38 @@ int is_delim(char c, char *delim)
 
 /**
  * _isalpha - Checks if a character is alphabetic.
- * @c: The character to check.
+ * @ch: The character to check.
  *
  * Return: 1 if the character is alphabetic, 0 otherwise.
  */
-int _isalpha(int c)
+int _isalpha(int ch)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
 }
 
 /**
  * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * @str: The string to be converted.
  *
  * Return: The converted integer value.
  */
-int _atoi(char *s)
+int _atoi(char *str)
 {
 	int sign = 1;
 	int result = 0;
 	int i = 0;
 
-	if (s[i] == '-')
+	if (str[i] == '-')
 	{
 		sign = -1;
 		i++;
 	}
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if (str[i] >= '0' && str[i] <= '9')
 		{
-			result = result * 10 + (s[i] - '0');
+			result = result * 10 + (strr[i] - '0');
 			i++;
 		}
 		else
