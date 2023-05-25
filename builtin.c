662dc90fcf826c5f9962bac6645636f8d9e58762
Nacer-eddine Kheireddine <109687458+Nacer-eddine1@users.noreply.github.com>
2023-05-25 20:13:51 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,38 +1,21 @@
 #include "shell.h"
-
-/**
- * _myhelp - it changes th current dir of the process
- * @info: used to maintain a constant function prototype
- * Return: Always 0 (Success)
- */
-int _myhelp(info_t *info)
-{
-	char **arg_ary;
-
-	arg_ary = info->argv;
-	_puts("help call works. Function is not implemented yet \n");
-	if (0)
-		_puts(*arg_ary);
-	return (0);
-}
-
 /**
  * _myexit -to exit the shell
- * @info: used to maintain a constant function prototype. it
- *   is the structure contains the potential argmnts
+ * @info: it is used to maintain a constant function prototype. it
+ *   is the structure that contains the potential arguments
  * Return: (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
-	int extchk;
+	int exitcheck;
 
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		extchk = _erratoi(info->argv[1]);
-		if (extchk == -1)
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
 		{
 			info->status = 2;
-			print_error(info, "Invalide number: ");
+			print_error(info, "Illegal number: ");
 			_eputs(info->argv[1]);
 			_eputchar('\n');
 			return (1);
@@ -45,33 +28,66 @@ int _myexit(info_t *info)
 }
 
 /**
- * _myhelp - it changes th current dir of the process
- * @info: used to maintain a constant function prototype
+ * _mycd - it changes the current directory
+ * @info: it is used to maintain a constant function prototype
  * Return: Always 0 (Success)
  */
-int _myhelp(info_t *info)
+int _mycd(info_t *info)
 {
-	char **arg_ary;
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
 
-	arg_ary = info->argv;
-	_puts("help call works. Function is not implemented yet \n");
-	if (0)
-		_puts(*arg_ary);
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
 	return (0);
 }
 
 /**
- * _myhelp - it changes th current dir of the process
- * @info: used to maintain a constant function prototype
+ * _myhelp - changes the current directory of the process
+ * @info: it is used to maintain a constant function prototype
  * Return: Always 0 (Success)
  */
 int _myhelp(info_t *info)
 {
-	char **arg_ary;
+	char **arg_array;
 
-	arg_ary = info->argv;
-	_puts("help call works. Function is not implemented yet \n");
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_ary);
+		_puts(*arg_array);
 	return (0);
 }
